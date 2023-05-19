5308740aa09b0121079733b25416475424cf9705
unknown <a.haghighi983@yahoo.com>
2023-05-20 01:38:12 +0330

    Anchor wrapper created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { Container } from "@mantine/core";
 import { ThemeProvider } from "./ThemeProvider";
 
 export default function App() {
   return (
     <ThemeProvider>
-      <Container />
     </ThemeProvider>
   );
 }
