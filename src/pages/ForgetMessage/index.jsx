06fd644955c3be0d1e5fc6df4f8f4205991b64ae
meisam9 <meisam949494@gmail.com>
2023-05-20 22:41:24 +0330

    forgetMessage added

--- src/pages/ForgetMessage/index.jsx
+++ src/pages/ForgetMessage/index.jsx
@@ -1,6 +1,24 @@
+import {Flex, Text} from '@mantine/core'
+import  Card  from "../../components/Card"
+import AuthLayout from "../../layout/AuthLayout"
+
 const ForgetMessage = () => {
     return (
-      <div>ForgetMessage</div>
+      <AuthLayout loginPage={false}>
+        <Card
+        miw="402px"
+        shadow="0px 12px 50px rgba(0, 0, 0, 0.18)"
+        radius="20px"
+      >
+        <Flex direction={"column"}
+          align="center"
+          p={"24px"}
+          justify="center">
+          <Text fz="32px" fw="600">فراموشی رمز عبور</Text>
+          <Text fz="14px" fw="400" mt="30px">لینک بازیابی رمز عبور برای شما ایمیل شد. لطفا ایمیل خود را بررسی کنید.</Text>
+        </Flex>
+      </Card>
+      </AuthLayout>
     )
   }
   
