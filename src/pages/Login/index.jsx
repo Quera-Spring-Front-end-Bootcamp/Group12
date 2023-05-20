8a0836ee322d82886106fdd662f62ccf8114c1e2
unknown <a.haghighi983@yahoo.com>
2023-05-20 23:13:28 +0330

    register page developed

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -13,23 +13,15 @@ const Login = () => {
         miw="402px"
         shadow="0px 12px 50px rgba(0, 0, 0, 0.18)"
         radius="20px"
+        p={"24px"}
       >
-        <Flex
-          direction={"column"}
-          align="center"
-          p={"24px"}
-          gap={"29px"}
-          bg={"#fff"}
-          style={{
-            borderRadius: "20px",
-          }}
-        >
+        <Flex direction={"column"} align="center" gap={"29px"} bg={"#fff"}>
           <Text fz={"32px"} fw="600" color="#000">
             به کوئرا تسک منیجر خوش برگشتی :)
           </Text>
           <TextInput
             label="ایمیل"
-            style={{ width: "100%" }}
+            w={"100%"}
             labelProps={{
               style: {
                 fontWeight: "400",
@@ -38,23 +30,26 @@ const Login = () => {
               },
             }}
           />
+
+          <PasswordInput
+            w={"100%"}
+            dir="ltr"
+            label="رمز عبور"
+            mb={"8px"}
+            fw={"400"}
+            fz={"50px"}
+            labelProps={{
+              style: {
+                fontWeight: "400",
+                marginBottom: "8px",
+                fontSize: "14px",
+              },
+            }}
+          />
+          <Anchor href="#">رمز عبور را فراموش کرده‌ای؟</Anchor>
+
           <div style={{ width: "100%" }}>
-            <PasswordInput
-              dir="ltr"
-              label="رمز عبور"
-              mb={"8px"}
-              labelProps={{
-                style: {
-                  fontWeight: "400",
-                  marginBottom: "8px",
-                  fontSize: "14px",
-                },
-              }}
-            />
-            <Anchor href="#">رمز عبور را فراموش کرده‌ای؟</Anchor>
-          </div>
-          <div style={{ width: "100%" }}>
-            <Button w={"100%"} fz="14px" fw="700">
+            <Button h={"40px"} w={"100%"} fz="14px" fw="700">
               ورود
             </Button>
             <Flex justify={"center"} mt="20px" gap="7px">
