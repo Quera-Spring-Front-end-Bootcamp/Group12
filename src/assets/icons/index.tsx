893e395a0e4a5816607aa10e2294348c6a02e824
unknown <a.haghighi983@yahoo.com>
2023-05-25 20:00:24 +0330

    directory of icons index file corrected

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,45 @@
+import { ReactComponent as Arrow } from './svg/Arrow-borderless.svg';
+import { ReactComponent as ArrowCircleBorder } from './svg/Arrow.svg';
+import { ReactComponent as Calender } from './svg/Calendar,Schedule.svg';
+import { ReactComponent as CheckmarkSqaure } from './svg/checkmark-sqaure.svg';
+import { ReactComponent as ColorPalette } from './svg/Color, Palette.svg';
+import { ReactComponent as Column } from './svg/column-artboard.svg';
+import { ReactComponent as Close } from './svg/Delete, Disabled.svg';
+import { ReactComponent as Exit } from './svg/Exit, Log out.svg';
+import { ReactComponent as TwoLineSetting } from './svg/Filter, Settings, Sort.svg';
+import { ReactComponent as Flag } from './svg/Flag.svg';
+import { ReactComponent as LeftArrow } from './svg/Left, Arrow.svg';
+import { ReactComponent as LinkBig } from './svg/link-big.svg';
+import { ReactComponent as CheckList } from './svg/List, Do list, Checkmark.svg';
+import { ReactComponent as Inbox } from './svg/mailbox-inbox-archive.svg';
+import { ReactComponent as Plus } from './svg/Plus-borderless.svg';
+import { ReactComponent as PlusSquare } from './svg/Plus, Add.svg';
+import { ReactComponent as Search } from './svg/search-loupe.svg';
+import { ReactComponent as Delete } from './svg/Trash, Delete, Bin.svg';
+import { ReactComponent as Dots } from './svg/dots-menu.svg';
+import { ReactComponent as Done } from './svg/Checkmark, Done, Check.svg';
+import { ReactComponent as JustifyRight } from './justify-right.svg';
+
+export {
+  Arrow,
+  ArrowCircleBorder,
+  Calender,
+  CheckmarkSqaure,
+  ColorPalette,
+  Column,
+  Close,
+  Exit,
+  TwoLineSetting,
+  Flag,
+  LeftArrow,
+  LinkBig,
+  CheckList,
+  Inbox,
+  Plus,
+  PlusSquare,
+  Search,
+  Delete,
+  Done,
+  Dots,
+  JustifyRight,
+};
