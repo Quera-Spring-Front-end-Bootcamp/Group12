1d2a3bc85671a172393ddaf491c10d6f8ca1eab0
unknown <a.haghighi983@yahoo.com>
2023-05-27 15:58:54 +0330

    boardview added to main layout

--- src/layout/MainLayout/TaskViews/BoardView/Boardview.tsx
+++ src/layout/MainLayout/TaskViews/BoardView/Boardview.tsx
@@ -1,46 +1,54 @@
-import { Flex, Text } from "@mantine/core";
+import { Flex, ScrollArea, Stack, Text } from "@mantine/core";
 import TaskListHeader from "../../../../components/TaskListHeader";
 import TaskCard from "../../../../components/TaskCard";
 
 const BoardView = () => {
   return (
     <>
-      <Flex gap={"lg"} >
-              <Flex direction={"column"} miw={'250px'} gap={"md"}>
-                  <TaskListHeader color="red">
-                      <Text>درحال انجام</Text>
-                  </TaskListHeader>
-                  <TaskCard/>
-                  <TaskCard/>
-                  <TaskCard/>
-                  <TaskCard/>
+      <Flex style={{flexShrink: '0'}} wrap={"nowrap"} gap={"lg"} mah={'100%'}>
+        <Flex style={{flexShrink: '0'}} wrap={"nowrap"} direction={"column"} miw={"250px"} gap={"md"} mah={'100%'}>
+          <TaskListHeader color="red">
+            <Text>درحال انجام</Text>
+          </TaskListHeader>
+          <ScrollArea type="never" mah={'100%'}>
+            <Flex
+              direction={"column"}
+              gap={"md"}
+            >
+              <TaskCard />
+              <TaskCard />
+              <TaskCard />
+              <TaskCard />
+              <TaskCard />
+            </Flex>
+          </ScrollArea>
         </Flex>
-              <Flex direction={"column"} miw={'250px'} gap={"md"}>
-                  <TaskListHeader color="red">
-                      <Text>درحال انجام</Text>
-                  </TaskListHeader>
-                  <TaskCard/>
-                  <TaskCard/>
-                  <TaskCard/>
-                  <TaskCard/>
+        <Flex style={{flexShrink: '0'}} direction={"column"} miw={"250px"} gap={"md"}>
+          <TaskListHeader color="red">
+            <Text>درحال انجام</Text>
+          </TaskListHeader>
+          <TaskCard />
+          <TaskCard />
+          <TaskCard />
+          <TaskCard />
         </Flex>
-              <Flex direction={"column"} miw={'250px'} gap={"md"}>
-                  <TaskListHeader color="red">
-                      <Text>درحال انجام</Text>
-                  </TaskListHeader>
-                  <TaskCard/>
-                  <TaskCard/>
-                  <TaskCard/>
-                  <TaskCard/>
+        <Flex style={{flexShrink: '0'}} direction={"column"} miw={"250px"} gap={"md"}>
+          <TaskListHeader color="red">
+            <Text>درحال انجام</Text>
+          </TaskListHeader>
+          <TaskCard />
+          <TaskCard />
+          <TaskCard />
+          <TaskCard />
         </Flex>
-              <Flex direction={"column"} miw={'250px'} gap={"md"}>
-                  <TaskListHeader color="red">
-                      <Text>درحال انجام</Text>
-                  </TaskListHeader>
-                  <TaskCard/>
-                  <TaskCard/>
-                  <TaskCard/>
-                  <TaskCard/>
+        <Flex style={{flexShrink: '0'}} direction={"column"} miw={"250px"} gap={"md"}>
+          <TaskListHeader color="red">
+            <Text>درحال انجام</Text>
+          </TaskListHeader>
+          <TaskCard />
+          <TaskCard />
+          <TaskCard />
+          <TaskCard />
         </Flex>
       </Flex>
     </>
--- src/layout/MainLayout/index.tsx
+++ src/layout/MainLayout/index.tsx
@@ -1,23 +1,25 @@
 import { AppShell } from '@mantine/core';
 import Sidebar from '../Sidebar';
 import List from '../../components/List';
-import MainPage from './MainHeader';
+import MainPage from './MainPage';
 import Button from '../../components/Button';
 import { PlusSquare } from '../../assets/icons';
 import SvgProvier from '../../assets/icons/SvgProvider';
+import BoardView from './TaskViews/BoardView/Boardview';
+import { ClassNames } from '@emotion/react';
 
 export default function MainLayout() {
   return (
     <>
       <AppShell bg={'#FAFBFC'}
-      className='relative'
+      className='relative h-screen overflow-scroll'
       navbar={<Sidebar/>}
 
       
 
       layout='alt'
     >
-      <MainPage list={<List/>}/>
+        <MainPage list={<List />} column={<BoardView/> } />
       <Button className='absolute bottom-6 left-4 z-10' leftIcon={<SvgProvier>
         <PlusSquare/>
       </SvgProvier>}>تسک جدید</Button>
