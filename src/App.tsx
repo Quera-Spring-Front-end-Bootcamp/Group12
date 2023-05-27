3d0b086ada1a2fd19d3aea1e94d0a717845803c5
shayan <shayan.shashaani02@gmail.com>
2023-05-26 17:58:05 -0800

    Removed extra

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { ThemeProvider } from './ThemeProvider';
+import MainLayout from './layout/MainLayout';
 import MainRouter from './router';
 
 export default function App() {
   return (
     <ThemeProvider>
-      <MainRouter />
+      <MainLayout />
     </ThemeProvider>
   );
 }
