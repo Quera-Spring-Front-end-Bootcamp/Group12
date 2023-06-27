4816d1bd0190fd0aa8d5ce2606c784467c6e2a06
unknown <a.haghighi983@yahoo.com>
2023-06-28 01:54:36 +0330

    add board in boardview developed

--- src/layout/MainLayout/TaskViews/BoardView/Boardview.tsx
+++ src/layout/MainLayout/TaskViews/BoardView/Boardview.tsx
@@ -1,4 +1,4 @@
-import { Flex, Skeleton } from '@mantine/core';
+import { Flex, Skeleton, Text } from '@mantine/core';
 import FilterTask from '../../../../components/FilterTasks';
 import Board from '../../../../components/Board/Board';
 import { useAppDispatch, useAppSelector } from '../../../../data/reduxHooks';
@@ -7,12 +7,19 @@ import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 import boardsSlice, { task } from '../../../../data/dataSlice/boardsSlice';
 import myAxios from '../../../../helpers/myAxios';
 import { useState } from 'react';
+import TaskListItem from '../../../../components/TaskListItem';
+import SvgProvier from '../../../../assets/icons/SvgProvider';
+import { Plus } from '../../../../assets/icons';
+import { useParams } from 'react-router';
+import AddBoardModal from '../../../../components/AddBoardModal';
+import { useDisclosure } from '@mantine/hooks';
 
 const BoardView = () => {
   const { isLoading } = useAppSelector((state) => state.boards);
   const storeBoards = useAppSelector((state) => state.boards.projectBoards);
   const projectName = useAppSelector((state) => state.boards.projectName);
-
+  const [boardOpened, { open: openboard, close: closeboard }] = useDisclosure(false);
+  const { projectID } = useParams();
   const [dragTask, setDragTask] = useState(false);
   const toggleDragTask = () => {
     setDragTask(!dragTask);
@@ -21,7 +28,9 @@ const BoardView = () => {
   const { updateBoards } = boardsSlice.actions;
   const onDragEnd = (result: DropResult) => {
     toggleDragTask();
+    console.log(result);
     if (!result.destination) return;
+    //delete task by dnd
     if (result.destination.droppableId === 'delete') {
       const sourceBoard = storeBoards.find((board) => board._id === result.source.droppableId);
       const updatedSourceBoard = {
@@ -42,11 +51,13 @@ const BoardView = () => {
       myAxios.delete(`/task/${result.draggableId}`);
       return;
     }
+    //when task is dropped in it's own position
     if (
       result.source.droppableId === result.destination?.droppableId &&
       result.source.index === result.destination?.index
     )
       return;
+    //change index of task in the same board
     if (result.source.droppableId === result.destination?.droppableId) {
       const board = storeBoards.find((board) => board._id === result.source.droppableId);
       const updatedTasks = board?.tasks.map((task) => {
@@ -104,10 +115,12 @@ const BoardView = () => {
           };
         return storeBoard;
       });
+      console.log(updatedBoards);
       dispatch(updateBoards(updatedBoards));
       myAxios.put(`/task/${result.draggableId}/position/${result.destination.index + 1}`);
       return;
     }
+    //change task board and index
     if (result.destination && result.source.droppableId !== result.destination.droppableId) {
       const destination = result.destination;
       const sourceBoard = storeBoards.find((board) => board._id === result.source.droppableId);
@@ -155,6 +168,7 @@ const BoardView = () => {
 
   return (
     <>
+      <AddBoardModal opened={boardOpened} onClose={closeboard} id={projectID} />
       <DragDropContext onBeforeCapture={toggleDragTask} onDragEnd={onDragEnd}>
         <FilterTask dragTask={dragTask} />
         {!(isLoading === 'seccuss') ? (
@@ -222,6 +236,21 @@ const BoardView = () => {
                   projectName={projectName}
                 />
               ))}
+              <div className="h-fit" onClick={openboard}>
+                <TaskListItem
+                  p={16}
+                  className="shrink-0 cursor-pointer hover:shadow-lg hover:translate-y-1 transition-all duration-100"
+                  h={45}
+                  w={256}
+                >
+                  <Flex h={'100%'} align={'center'}>
+                    <SvgProvier>
+                      <Plus />
+                    </SvgProvier>
+                    <Text>ساختن ستون جدید</Text>
+                  </Flex>
+                </TaskListItem>
+              </div>
             </ScrollContainer>
           </Flex>
         )}
